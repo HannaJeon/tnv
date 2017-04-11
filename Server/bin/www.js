f6359aace51af268aea21b8a878231c153a4cc59
hoemoon <hoemoon.song@gmail.com>
2017-04-11 10:36:06 +0900

    gitignore update

--- Server/bin/www.js
+++
@@ -1,6 +0,0 @@
-var app = require('../index');
-
-app.set('port', process.env.PORT || 8000);
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port '+ server.address.port);
-});
